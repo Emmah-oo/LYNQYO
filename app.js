2b2d234793fa69333bb785880fb8422925eaaf35
Emmah-oo <emmaochonogor715@gmail.com>
2022-07-06 21:16:18 -0700

    SEO DONE ✔

--- app.js
+++ app.js
@@ -30,3 +30,42 @@ const burger = document.querySelector(".burger");
 burger.addEventListener("click", () => {
   navLinks.classList.toggle("active");
 });
+
+
+(function () {
+  const second = 1000,
+    minute = second * 60,
+    hour = minute * 60,
+    day = hour * 24;
+
+  let today = new Date(),
+    dd = String(today.getDate()).padStart(2, "0"),
+    mm = String(today.getMonth() + 1).padStart(2, "0"),
+    yyyy = today.getFullYear(),
+    nextYear = yyyy + 1,
+    dayMonth = "07/20/",
+    launchDay = dayMonth + yyyy;
+
+  today = mm + "/" + dd + "/" + yyyy;
+  if (today > launchDay) {
+    launchDay = dayMonth + nextYear;
+  }
+  //end
+
+  const countDown = new Date(launchDay).getTime(),
+    x = setInterval(function () {
+      const now = new Date().getTime(),
+        distance = countDown - now;
+
+      document.querySelector(".countdown .days").innerText = Math.floor(distance / day);
+      (document.querySelector(".countdown .hours").innerText = Math.floor(
+        ((distance % day) / hour)
+      )),
+        (document.querySelector(".countdown .minutes").innerText = Math.floor(
+          (distance % hour) / minute
+        )),
+        (document.querySelector(".countdown .secs").innerText = Math.floor(
+          (distance % minute) / second
+        ));
+    }, 0);
+})();
